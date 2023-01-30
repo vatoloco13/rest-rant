83fcd20f21eaf2ce9a9721f9404e2053b3315a4c
juan luiz barraza <99239515+vatoloco13@users.noreply.github.com>
2023-01-29 22:34:00 -0800

    asdf

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-
+app.use('/places', require('./controllers/places'))
 app.get('/', (req, res) => {
     res.send('hello world');
 })
