2549c0c1f5528cc5f55f31d8cbf25f6060c0f62e
juan luiz barraza <99239515+vatoloco13@users.noreply.github.com>
2023-01-30 20:39:17 -0800

    final push

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-require('dotenv').config();
 const express = require('express');
+require('dotenv').config()
 const app = express();
+app.use('/places',require('./controllers/places'))
 
-app.use('/places', require('./controllers/places'))
-
-app.get('/', (req, res) => {
-    res.send('hello world');
-})
-app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
-})
+app.get('/', function(req, res) {
+    res.send("hello world!");
+});
+app.get('*',(req, res) => {
+    res.status(404).send('<h1>404 Not Found</h1>')})
+   
 app.listen(process.env.PORT)
